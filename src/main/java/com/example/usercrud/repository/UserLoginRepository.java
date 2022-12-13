e2bc880ab74e5f9556974a81bb46e38838b686b2
duchai159 <sam@google.com>
2022-12-13 09:48:14 +0700

    first commit

--- src/main/java/com/example/usercrud/repository/UserLoginRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.usercrud.repository;
-
-import com.example.usercrud.entity.UserLogin;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserLoginRepository extends JpaRepository<UserLogin, Long> {
-    UserLogin findByUsername(String username);
-}
